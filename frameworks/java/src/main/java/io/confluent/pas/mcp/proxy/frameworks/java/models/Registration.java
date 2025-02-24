2edd6bae5efa120bfe944c8af58befc07ab1b19e
Pascal Vantrepote <pascal@confluent.io>
2025-02-24 16:38:39 -0500

    Adding missing schema

--- frameworks/java/src/main/java/io/confluent/pas/mcp/proxy/frameworks/java/models/Registration.java
+++ frameworks/java/src/main/java/io/confluent/pas/mcp/proxy/frameworks/java/models/Registration.java
@@ -1,8 +1,69 @@
 package io.confluent.pas.mcp.proxy.frameworks.java.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.confluent.kafka.schemaregistry.annotations.Schema;
 import io.confluent.pas.mcp.common.services.models.AbstractRegistration;
 import lombok.NoArgsConstructor;
 
+@Schema(value = """
+        {
+           "properties":{
+              "correlationIdFieldName":{
+                 "connect.index":4,
+                 "oneOf":[
+                    {
+                       "type":"null"
+                    },
+                    {
+                       "type":"string"
+                    }
+                 ]
+              },
+              "description":{
+                 "connect.index":1,
+                 "oneOf":[
+                    {
+                       "type":"null"
+                    },
+                    {
+                       "type":"string"
+                    }
+                 ]
+              },
+              "name":{
+                 "connect.index":0,
+                 "type":"string"
+              },
+              "requestTopicName":{
+                 "connect.index":2,
+                 "oneOf":[
+                    {
+                       "type":"null"
+                    },
+                    {
+                       "type":"string"
+                    }
+                 ]
+              },
+              "responseTopicName":{
+                 "connect.index":3,
+                 "oneOf":[
+                    {
+                       "type":"null"
+                    },
+                    {
+                       "type":"string"
+                    }
+                 ]
+              }
+           },
+           "required":[
+              "name"
+           ],
+           "title":"Record",
+           "type":"object"
+        }""", refs = {})
+@JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor
 public class Registration extends AbstractRegistration {
 
